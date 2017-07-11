f8a84874a42878fd1d2bff7090d012d7dc7ac762
Tomáš Korený <pumaslab@gmail.com>
2017-07-11 17:14:35 +0200

    Parameters

--- main.go
+++ main.go
@@ -23,7 +23,6 @@ import (
 	"net/http"
 	"os"
 	"strings"
-	"time"
 )
 
 //var executor *graphql.Executor
@@ -59,11 +58,12 @@ func authMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func test() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(fmt.Sprint(r.Context().Value("user_id"))))
-	})
-}
+//
+//func test() http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		w.Write([]byte(fmt.Sprint(r.Context().Value("user_id"))))
+//	})
+//}
 
 func queryHand() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -95,138 +95,139 @@ func queryHand() http.Handler {
 
 	})
 }
-func main1() {
-	//r := resolvers.GetResolvers()
-	var err error
-	//executor, err = graphql.NewExecutor(resolvers.Schema, "Query", "Mutation", r)
 
-	//db, err := sql.Open("mysql", "root:password@tcp(db:3306)/database")
-	db, err := sql.Open("mysql", "root:pass@/database")
-	if err != nil {
-		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
-	}
-	defer db.Close()
-	database.Connect(db)
-
-	//CORS STARTS HERE - DEV ONL	Y
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Write(page)
-	}))
-
-	mux.Handle("/test", authMiddleware(test()))
-	//mux.Handle("/query", authMiddleware(queryHand()))
-	//mux.Handle("/query", authMiddleware(queryHand()))
-	//mux.Handle("/query", queryHand())
-
-	mux.HandleFunc("/login", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		var m interface{}
-		err = json.Unmarshal(body, &m)
-
-		if m != nil {
-			if m.(map[string]interface{})["pass"] != nil && m.(map[string]interface{})["user"] != nil {
-				h := md5.New()
-				io.WriteString(h, m.(map[string]interface{})["pass"].(string))
-				var username string
-				var pass sql.NullString
-				var id []uint8
-				rows, err := database.Db.Query("select id, username, pass from users")
-				if err != nil {
-					log.Fatal(err)
-				}
-				defer rows.Close()
-				for rows.Next() {
-					err := rows.Scan(&id, &username, &pass)
-					if err != nil {
-						log.Fatal(err)
-					}
-					if strings.ToLower(m.(map[string]interface{})["user"].(string)) == strings.ToLower(username) {
-						token := randToken()
-						if pass.Valid {
-							passhash := new(bytes.Buffer)
-							fmt.Fprintf(passhash, "%x", h.Sum(nil)) //cast pass hash to var
-							if passhash.String() == pass.String {
-								_, err := database.Db.Exec("INSERT INTO logins (user_id, token) VALUES (?, ?)", id, token) //Save Token to db
-								if err != nil {
-									panic(err)
-								}
-								resp, err := json.Marshal(map[string]interface{}{"token": token})
-								w.Write(resp) //USER IS LOGIN, send him token
-								return
-							}
-						} else {
-							_, err := database.Db.Exec("INSERT INTO logins (user_id, token) VALUES (?, ?)", id, token) //Save Token to db
-							if err != nil {
-								panic(err)
-							}
-							resp, err := json.Marshal(map[string]interface{}{"token": token, "first": true})
-							w.Write(resp) //USER IS LOGIN, send him token
-							return
-						}
-
-					}
-				}
-				err = rows.Err()
-				resp, err := json.Marshal(map[string]interface{}{"error": "unknown-user-or-pass"})
-				w.Write(resp)
-				return
-			}
-		}
-		resp, err := json.Marshal(map[string]interface{}{"error": err})
-		w.Write(resp)
-	}))
-
-	mux.HandleFunc("/logout", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("token") != "" {
-			_, err := database.Db.Exec("DELETE FROM logins WHERE token = ?", r.Header.Get("token")) //delete token
-			if err != nil {
-				panic(err)
-			}
-			resp, _ := json.Marshal(map[string]interface{}{"success": true})
-			w.Write(resp)
-		} else {
-			resp, _ := json.Marshal(map[string]interface{}{"error": "no_header"})
-			w.Write(resp)
-		}
-
-	}))
-
-	// cors.Default() setup the middleware with default options being
-	// all origins accepted with simple methods (GET, POST). See
-	// documentation below for more options.
-	handler := cors.AllowAll().Handler(mux)
-
-	//CORS ENDS HERE - DEV ONLY
-
-	//PRODUCTION START
-
-	//http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	//	w.Write(page)
-	//}))
-	//
-	//http.Handle("/query", &relay.Handler{Schema: schema})
-
-	//PRODUCTION END
-
-	//SEED DB
-	time.Sleep(5000 * time.Millisecond)
-	var c int
-	err = database.Db.QueryRow("select count(id) as c from users").Scan(&c)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if c == 0 {
-		_, err := database.Db.Exec("INSERT INTO users (username, first_name, last_name, bg_color, color, email) VALUES (?, ?, ?, ?, ?, ?)", "lordpuma", "Tomáš", "Korený", "#000000", "#FFFFFF", "")
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	log.Fatal(http.ListenAndServe(":8080", handler))
-
-}
+//func main1() {
+//	//r := resolvers.GetResolvers()
+//	var err error
+//	//executor, err = graphql.NewExecutor(resolvers.Schema, "Query", "Mutation", r)
+//
+//	//db, err := sql.Open("mysql", "root:password@tcp(db:3306)/database")
+//	db, err := sql.Open("mysql", "root:pass@/database")
+//	if err != nil {
+//		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+//	}
+//	defer db.Close()
+//	database.Connect(db)
+//
+//	//CORS STARTS HERE - DEV ONL	Y
+//	mux := http.NewServeMux()
+//
+//	mux.HandleFunc("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		w.Write(page)
+//	}))
+//
+//	mux.Handle("/test", authMiddleware(test()))
+//	//mux.Handle("/query", authMiddleware(queryHand()))
+//	//mux.Handle("/query", authMiddleware(queryHand()))
+//	//mux.Handle("/query", queryHand())
+//
+//	mux.HandleFunc("/login", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		body, err := ioutil.ReadAll(r.Body)
+//		var m interface{}
+//		err = json.Unmarshal(body, &m)
+//
+//		if m != nil {
+//			if m.(map[string]interface{})["pass"] != nil && m.(map[string]interface{})["user"] != nil {
+//				h := md5.New()
+//				io.WriteString(h, m.(map[string]interface{})["pass"].(string))
+//				var username string
+//				var pass sql.NullString
+//				var id []uint8
+//				rows, err := database.Db.Query("select id, username, pass from users")
+//				if err != nil {
+//					log.Fatal(err)
+//				}
+//				defer rows.Close()
+//				for rows.Next() {
+//					err := rows.Scan(&id, &username, &pass)
+//					if err != nil {
+//						log.Fatal(err)
+//					}
+//					if strings.ToLower(m.(map[string]interface{})["user"].(string)) == strings.ToLower(username) {
+//						token := randToken()
+//						if pass.Valid {
+//							passhash := new(bytes.Buffer)
+//							fmt.Fprintf(passhash, "%x", h.Sum(nil)) //cast pass hash to var
+//							if passhash.String() == pass.String {
+//								_, err := database.Db.Exec("INSERT INTO logins (user_id, token) VALUES (?, ?)", id, token) //Save Token to db
+//								if err != nil {
+//									panic(err)
+//								}
+//								resp, err := json.Marshal(map[string]interface{}{"token": token})
+//								w.Write(resp) //USER IS LOGIN, send him token
+//								return
+//							}
+//						} else {
+//							_, err := database.Db.Exec("INSERT INTO logins (user_id, token) VALUES (?, ?)", id, token) //Save Token to db
+//							if err != nil {
+//								panic(err)
+//							}
+//							resp, err := json.Marshal(map[string]interface{}{"token": token, "first": true})
+//							w.Write(resp) //USER IS LOGIN, send him token
+//							return
+//						}
+//
+//					}
+//				}
+//				err = rows.Err()
+//				resp, err := json.Marshal(map[string]interface{}{"error": "unknown-user-or-pass"})
+//				w.Write(resp)
+//				return
+//			}
+//		}
+//		resp, err := json.Marshal(map[string]interface{}{"error": err})
+//		w.Write(resp)
+//	}))
+//
+//	mux.HandleFunc("/logout", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		if r.Header.Get("token") != "" {
+//			_, err := database.Db.Exec("DELETE FROM logins WHERE token = ?", r.Header.Get("token")) //delete token
+//			if err != nil {
+//				panic(err)
+//			}
+//			resp, _ := json.Marshal(map[string]interface{}{"success": true})
+//			w.Write(resp)
+//		} else {
+//			resp, _ := json.Marshal(map[string]interface{}{"error": "no_header"})
+//			w.Write(resp)
+//		}
+//
+//	}))
+//
+//	// cors.Default() setup the middleware with default options being
+//	// all origins accepted with simple methods (GET, POST). See
+//	// documentation below for more options.
+//	handler := cors.AllowAll().Handler(mux)
+//
+//	//CORS ENDS HERE - DEV ONLY
+//
+//	//PRODUCTION START
+//
+//	//http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//	//	w.Write(page)
+//	//}))
+//	//
+//	//http.Handle("/query", &relay.Handler{Schema: schema})
+//
+//	//PRODUCTION END
+//
+//	//SEED DB
+//	time.Sleep(5000 * time.Millisecond)
+//	var c int
+//	err = database.Db.QueryRow("select count(id) as c from users").Scan(&c)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	if c == 0 {
+//		_, err := database.Db.Exec("INSERT INTO users (username, first_name, last_name, bg_color, color, email) VALUES (?, ?, ?, ?, ?, ?)", "lordpuma", "Tomáš", "Korený", "#000000", "#FFFFFF", "")
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	}
+//
+//	log.Fatal(http.ListenAndServe(":8080", handler))
+//
+//}
 
 var page = []byte(`
 <!DOCTYPE html>
